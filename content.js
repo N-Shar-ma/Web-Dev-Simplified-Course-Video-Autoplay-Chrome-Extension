9a798d288bec357810eaa35c8105a74670180614
Neh-Sharma <bablinaneh@gmail.com>
2020-11-30 23:31:23 +0530

    using chrome storage instead of passing messages

--- content.js
+++ content.js
@@ -1,10 +1,5 @@
-let autoplay = true
-let delay = 5000
-
-chrome.runtime.onMessage.addListener( function (req) {
-    autoplay = req.autoplay
-    delay = req.delay * 1000
-})
+let autoplay
+let delay
 
 const timer = setInterval(() => {
     const videoElem = document.getElementsByTagName("video")[0];
@@ -15,10 +10,20 @@ const timer = setInterval(() => {
     if(!nextButton) return
     console.log("Next Button Found")
     videoElem.addEventListener("ended", () => {
-        if(!autoplay) return
-        setTimeout(() => {
-            console.log("clicked next")
-            window.location = nextButton.href
-        }, delay)
+        chrome.storage.sync.get(["WDSCourseAutoplay"], function (data) {
+            data = data.WDSCourseAutoplay
+            if (data) {
+                autoplay = data.autoplay
+                delay = data.delay * 1000
+            } else {
+                autoplay = true
+                delay = 5000
+            }
+            if(!autoplay) return console.log("Autoplay turned off")
+            setTimeout(() => {
+                console.log("clicked Next")
+                window.location = nextButton.href
+            }, delay)
+        })
     })
 }, 500)
--- popup.js
+++ popup.js
@@ -1,13 +1,23 @@
 const inputs = [...document.querySelectorAll("input")]
 
-inputs.forEach( input => input.addEventListener("change", sendMessage))
+inputs.forEach( input => input.addEventListener("change", storeData))
 
-function sendMessage () {
+chrome.storage.sync.get(["WDSCourseAutoplay"], function (data) {
+    data = data.WDSCourseAutoplay
+    if (data) {
+        inputs[0].checked = data.autoplay
+        inputs[1].value = data.delay
+    } else {
+        inputs[0].checked = true
+        inputs[1].value = 5
+        storeData()
+    }
+})
+
+function storeData () {
     const data = {
         autoplay: inputs[0].checked,
         delay: inputs[1].valueAsNumber
     }
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, data)
-    })
+    chrome.storage.sync.set({ "WDSCourseAutoplay": data})
 }
